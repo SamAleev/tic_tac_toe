99e2d046cec8854716fc5f2da5a05ffd3d3c5950
Aleksandr Samrinov <alesam@yandex.ru>
2025-08-05 20:43:02 +0300

    final

--- game.py
+++ game.py
@@ -1,8 +1,67 @@
+# game.py
+
 from gameparts import Board
+from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
 
-game = Board()
-game.display()
-game.make_move(1, 1, 'X')
-print('Ход сделан!')
-game.display()
+def main():
+    game = Board()
+    # Первыми ходят крестики.
+    current_player = 'X'
+    # Это флаговая переменная. По умолчанию игра запущена и продолжается.
+    running = True
+    game.display()
+
+    # Тут запускается основной цикл игры.
+    while running:
+
+        print(f'Ход делают {current_player}')
+
+        # Запускается бесконечный цикл.
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    # Вот тут выбрасывается новое исключение.
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+
+        # Теперь для установки значения на поле само значение берётся
+        # из переменной current_player.
+        game.make_move(row, column, current_player)
+        game.display()
+        # После каждого хода надо делать проверку на победу и на ничью.
+        if game.check_win(current_player):
+            print(f'Победили {current_player}.')
+            running = False
+        elif game.is_board_full():
+            print('Ничья!')
+            running = False
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+if __name__ == '__main__':
+    main()
--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,8 +1,15 @@
-# parts.py
+# gameparts/parts.py
 
 class Board:
+    """Класс, который описывает игровое поле."""
+
+    # Новый атрибут.
+    field_size = 3
+
     def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.board = [
+            [' ' for _ in range(self.field_size)] for _ in range(self.field_size)
+        ]
 
     def make_move(self, row, col, player):
         self.board[row][col] = player
@@ -11,3 +18,41 @@ class Board:
         for row in self.board:
             print('|'.join(row))
             print('-' * 5)
+
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ...игра продолжается.
+                    return False
+        # Иначе - ничья!
+        return True
+
+        # Этот метод будет определять победу.
+    def check_win(self, player):
+        # Проверка по горизонталям и вертикалям
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                    all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+
+        # Проверка по диагоналям
+        if (all([self.board[i][i] == player for i in range(self.field_size)]) or
+                all([self.board[i][self.field_size - 1 - i] == player for i in range(self.field_size)])):
+            return True
+
+        return False
+
+
+    # Переопределяем метод __str__.
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        )
+
+
+print(Board.__doc__)
