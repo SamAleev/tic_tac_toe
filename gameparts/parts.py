90db022153df21adb3bdce9480de2ac8275ecb83
Aleksandr Samrinov <alesam@yandex.ru>
2025-08-06 14:20:06 +0300

    garfics

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -14,10 +14,12 @@ class Board:
     def make_move(self, row, col, player):
         self.board[row][col] = player
 
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
+# только для консольной версии
+
+    # def display(self):
+    #     for row in self.board:
+    #         print('|'.join(row))
+    #         print('-' * 5)
 
     def is_board_full(self):
         # Цикл проходится по всем столбцам игрового поля.
